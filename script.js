f45815ed7e9d5012d2b4860165c38d50395f7e0b
sliker92 <sliker1992@gmail.com>
2019-08-26 02:09:50 +0300

    menu for mobile

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let svg = document.getElementById('frame');
+document.querySelector('.menu__mobile-burger').addEventListener('click', showMenu, false);
 
+function showMenu() {
+  const item = document.getElementsByTagName('span');
+  const list = document.querySelector('.menu_mobile_wrapper');
+  if (item[0].className === 'burgerline') {
+    item[0].className = 'open';
+    list.style.display = 'block';
+  } else {
+    item[0].className = 'burgerline';
+    list.style.display = 'none';
+  }
+}
